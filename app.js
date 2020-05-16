b303659bba26a525409844bc604c3ebbc13a0063
nstratford76 <nstratford76@gmail.com>
2020-05-16 14:50:00 -0600

    My prove04 assignment with mongodb

--- app.js
+++ app.js
@@ -34,7 +34,35 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+const cors = require('cors') // Place this with other requires (like 'path' and 'express')
+const corsOptions = {
+    origin: "https://great-books-store.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://nstratford_test:Brandnewrules76@cluster0-hi2mo.mongodb.net/shop?retryWrites=true&w=majority";
+
+
+mongoose
+  .connect(
+    MONGODB_URL, options
+  )
+  .then(result => {
+     // This should be your user handling code implement following the course videos
+    app.listen(PORT);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 mongoConnect(() => {
   
